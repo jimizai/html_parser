e417e4ddd5801d9edd78ea3e8a40ac8d88c685bc
dingyike.dyk <dingyike.dyk@alibaba-inc.com>
2021-06-17 13:45:51 +0800

    feat: add complex html tests

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use html_parser::Scanner;
 use std::fs;
 
 fn main() {
-    let text = fs::read_to_string("index.html").unwrap();
+    let text = fs::read_to_string("test.html").unwrap();
     let mut scanner = Scanner::new(text.as_bytes());
     let data = scanner.parse();
-    println!("{:?}", data);
+    let node_tree = scanner.lexer(data);
+    println!("{:?}", node_tree);
 }
