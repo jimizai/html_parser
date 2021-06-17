38e0dc04ba70cc8f8e2a4dbed8e9eaefcdf764a6
dyk <359743984@qq.com>
2021-06-17 13:52:54 +0800

    fix: move test html to tests

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use html_parser::Scanner;
 use std::fs;
 
 fn main() {
-    let text = fs::read_to_string("test.html").unwrap();
+    let text = fs::read_to_string("tests/test.html").unwrap();
     let mut scanner = Scanner::new(text.as_bytes());
     let data = scanner.parse();
     let node_tree = scanner.lexer(data);
