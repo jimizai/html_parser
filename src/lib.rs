a147ef25ed37bc25d9e13d35cdac3b731b7bc90b
dyk <359743984@qq.com>
2021-06-18 13:32:56 +0800

    feat: add annocation parse

--- src/lib.rs
+++ src/lib.rs
@@ -58,6 +58,7 @@ pub enum Tokenizer {
     EndTag,
     Text,
     Attribute,
+    Annocation,
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
@@ -67,16 +68,25 @@ pub struct Token<'a> {
 }
 
 impl<'a> Token<'a> {
-    pub fn new(value: &'a str, is_tag: bool, is_end: bool, is_attribute: bool) -> Token {
-        let tokenizer = match is_tag {
-            true => match is_end {
-                true => Tokenizer::EndTag,
-                false => match is_attribute {
-                    false => Tokenizer::Tag,
-                    true => Tokenizer::Attribute,
+    pub fn new(
+        value: &'a str,
+        is_tag: bool,
+        is_end: bool,
+        is_attribute: bool,
+        is_annotation: bool,
+    ) -> Token {
+        let tokenizer = match is_annotation {
+            true => Tokenizer::Annocation,
+            false => match is_tag {
+                true => match is_end {
+                    true => Tokenizer::EndTag,
+                    false => match is_attribute {
+                        false => Tokenizer::Tag,
+                        true => Tokenizer::Attribute,
+                    },
                 },
+                _ => Tokenizer::Text,
             },
-            _ => Tokenizer::Text,
         };
         Token { tokenizer, value }
     }
@@ -100,77 +110,141 @@ impl<'a> Scanner<'a> {
         let mut is_string = false;
         let mut has_text = false;
         let mut ignore_once = false;
+        let mut is_annotation = false;
 
         while self.end > self.position {
             let byte = self.bytes[self.position];
-            match byte {
-                b'<' => {
-                    if self.start_position != 0 && self.position != self.start_position {
-                        let text = std::str::from_utf8(self.get_bytes()).unwrap();
-                        tokens.push(Token::new(text, is_tag, is_end, is_attribute));
-                        self.start_position = 0;
-                    }
-                    is_tag = true;
-                    ignore_once = false;
-                }
-                b'>' => {
-                    if self.start_position != 0 {
-                        let text = std::str::from_utf8(self.get_bytes()).unwrap();
-                        tokens.push(Token::new(text, is_tag, is_end, is_attribute));
-                        self.start_position = 0;
-                    } else if is_end {
-                        if !ignore_once {
-                            tokens.push(Token::new("", is_tag, is_end, is_attribute));
-                        } else {
-                            ignore_once = false;
+            match is_annotation {
+                true => match byte {
+                    b'-' => {
+                        let next_byte = self.bytes[self.position + 1];
+                        let third_byte = self.bytes[self.position + 2];
+                        if next_byte == b'-' && third_byte == b'>' {
+                            let text = std::str::from_utf8(self.get_bytes()).unwrap();
+                            tokens.push(Token::new(
+                                text,
+                                is_tag,
+                                is_end,
+                                is_attribute,
+                                is_annotation,
+                            ));
+                            self.start_position = 0;
+                            is_annotation = false;
+                            self.position += 2;
+                            continue;
                         }
                     }
-                    is_tag = false;
-                    is_end = false;
-                    is_attribute = false;
-                }
-                b'/' => {
-                    if is_string {
-                        self.position += 1;
-                        continue;
-                    }
-                    if is_tag {
-                        is_end = true;
-                    }
-                    self.start_position = self.position + 1;
-                }
-                b'\n' | b'\r' | b' ' => {
-                    if is_string == true {
-                        self.position += 1;
-                        continue;
-                    }
-                    if !has_text {
-                        self.position += 1;
-                        self.start_position = 0;
-                        continue;
-                    } else if is_tag {
-                        if is_end {
-                            ignore_once = true;
+                    _ => {}
+                },
+                false => match byte {
+                    b'<' => {
+                        if self.start_position != 0 && self.position != self.start_position {
+                            let text = std::str::from_utf8(self.get_bytes()).unwrap();
+                            tokens.push(Token::new(
+                                text,
+                                is_tag,
+                                is_end,
+                                is_attribute,
+                                is_annotation,
+                            ));
+                            self.start_position = 0;
                         }
-                        let text = std::str::from_utf8(self.get_bytes()).unwrap();
-                        tokens.push(Token::new(text, is_tag, is_end, is_attribute));
-                        is_attribute = true;
-                        has_text = false;
-                        self.start_position = 0;
+                        is_tag = true;
+                        ignore_once = false;
                     }
-                }
-                b'"' => {
-                    if self.start_position == 0 && !is_string {
-                        self.start_position = self.position;
+                    b'!' => {
+                        if is_tag {
+                            let next_byte = self.bytes[self.position + 1];
+                            let third_byte = self.bytes[self.position + 2];
+                            if next_byte == b'-' && third_byte == b'-' {
+                                self.start_position = self.position + 3;
+                                is_annotation = true;
+                                is_tag = false;
+                                is_end = false;
+                                is_attribute = false;
+                                is_string = false;
+                                has_text = false;
+                                ignore_once = false;
+                            }
+                        }
                     }
-                    is_string = !is_string
-                }
-                _ => {
-                    if self.start_position == 0 {
-                        has_text = true;
-                        self.start_position = self.position;
+                    b'>' => {
+                        if self.start_position != 0 {
+                            let text = std::str::from_utf8(self.get_bytes()).unwrap();
+                            tokens.push(Token::new(
+                                text,
+                                is_tag,
+                                is_end,
+                                is_attribute,
+                                is_annotation,
+                            ));
+                            self.start_position = 0;
+                        } else if is_end {
+                            if !ignore_once {
+                                tokens.push(Token::new(
+                                    "",
+                                    is_tag,
+                                    is_end,
+                                    is_attribute,
+                                    is_annotation,
+                                ));
+                            } else {
+                                ignore_once = false;
+                            }
+                        }
+                        is_tag = false;
+                        is_end = false;
+                        is_attribute = false;
                     }
-                }
+                    b'/' => {
+                        if is_string {
+                            self.position += 1;
+                            continue;
+                        }
+                        if is_tag {
+                            is_end = true;
+                        }
+                        self.start_position = self.position + 1;
+                    }
+                    b'\n' | b'\r' | b' ' => {
+                        if is_string {
+                            self.position += 1;
+                            continue;
+                        }
+                        if !has_text {
+                            self.position += 1;
+                            self.start_position = 0;
+                            continue;
+                        } else if is_tag {
+                            if is_end {
+                                ignore_once = true;
+                            }
+                            let text = std::str::from_utf8(self.get_bytes()).unwrap();
+                            tokens.push(Token::new(
+                                text,
+                                is_tag,
+                                is_end,
+                                is_attribute,
+                                is_annotation,
+                            ));
+                            is_attribute = true;
+                            has_text = false;
+                            self.start_position = 0;
+                        }
+                    }
+                    b'"' => {
+                        if self.start_position == 0 && !is_string {
+                            self.start_position = self.position;
+                        }
+                        is_string = !is_string
+                    }
+                    _ => {
+                        if self.start_position == 0 {
+                            has_text = true;
+                            self.start_position = self.position;
+                        }
+                    }
+                },
             }
             self.position += 1;
         }
@@ -220,6 +294,7 @@ impl<'a> Scanner<'a> {
                         .set_attributes(tokens.get(0).unwrap_or(&""), tokens.get(1).unwrap_or(&""));
                 }
                 Tokenizer::Text => trees.last_mut().unwrap().set_text(token.value),
+                _ => {}
             }
         }
         node_tree
