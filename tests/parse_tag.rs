8167ce4b4aa94bde54dff567fbcb09d779216f95
dyk <359743984@qq.com>
2021-06-18 13:33:05 +0800

    test: add annocation parse test

--- tests/parse_tag.rs
+++ tests/parse_tag.rs
@@ -1,5 +1,11 @@
 use html_parser::{Scanner, Token, Tokenizer::*};
 
+fn assert_eq_tokens(mut target: Vec<Token>, mut result: Vec<Token>) {
+    for _ in 0..target.len() {
+        assert_eq!(target.pop().unwrap(), result.pop().unwrap())
+    }
+}
+
 #[test]
 fn single_tag_close() {
     let text = b" <div /> ";
@@ -7,9 +13,12 @@ fn single_tag_close() {
     let tokens = scanner.parse();
     assert_eq!(
         *tokens.get(0).unwrap(),
-        Token::new("div", true, false, false)
+        Token::new("div", true, false, false, false)
     );
-    assert_eq!(*tokens.get(1).unwrap(), Token::new("", true, true, false))
+    assert_eq!(
+        *tokens.get(1).unwrap(),
+        Token::new("", true, true, false, false)
+    )
 }
 
 #[test]
@@ -20,9 +29,12 @@ fn loose_tag_close() {
     println!("{:?}", tokens);
     assert_eq!(
         *tokens.get(0).unwrap(),
-        Token::new("div", true, false, false)
+        Token::new("div", true, false, false, false)
     );
-    assert_eq!(*tokens.get(1).unwrap(), Token::new("", true, true, false));
+    assert_eq!(
+        *tokens.get(1).unwrap(),
+        Token::new("", true, true, false, false)
+    );
 }
 
 #[test]
@@ -71,4 +83,56 @@ fn loose_tag() {
     for _ in 0..tokens.len() {
         assert_eq!(tokens.pop().unwrap(), result.pop().unwrap())
     }
+}
+
+#[test]
+fn tag_annotations() {
+    let text = r#"<li name="attr tets" test="sda">test</li>
+    <!-- 
+      <li>test</li>
+     -->
+    <li>test</li>"#;
+
+    let result = [
+        Token {
+            tokenizer: Tag,
+            value: "li",
+        },
+        Token {
+            tokenizer: Attribute,
+            value: "name=\"attr tets\"",
+        },
+        Token {
+            tokenizer: Attribute,
+            value: "test=\"sda\"",
+        },
+        Token {
+            tokenizer: Text,
+            value: "test",
+        },
+        Token {
+            tokenizer: EndTag,
+            value: "li",
+        },
+        Token {
+            tokenizer: Annocation,
+            value: " \n      <li>test</li>\n     ",
+        },
+        Token {
+            tokenizer: Tag,
+            value: "li",
+        },
+        Token {
+            tokenizer: Text,
+            value: "test",
+        },
+        Token {
+            tokenizer: EndTag,
+            value: "li",
+        },
+    ];
+
+    let mut scanner = Scanner::new(text.as_bytes());
+    let tokens = scanner.parse();
+    assert_eq_tokens(tokens, result.to_vec());
 }
